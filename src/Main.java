524ccda2d13fa51630693eb293aa180f61e1e40f
Alexsandro <contadetestealexsandro@gmail.com>
2025-08-14 16:59:29 -0300

    "Atualizando codigo."

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,22 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         FilePersistence persistence = new IOFIlePersistence("user.csv");
-        persistence.write("Alex;Alex@lucas.com;22/04/19983");
+        persistence.write("Alex;alex@lucas.com;22/04/1983");
+        System.out.println("========================================");
+        persistence.write("Rejane;rejane@rejane.com;25/07/1985");
+        System.out.println("========================================");
+        persistence.write("Isabella;isa@bella.com;02/08/2011");
+        System.out.println("========================================");
+        System.out.println(persistence.findAll());
+        System.out.println("========================================");
+        System.out.println(persistence.remove("/25/19"));
+        System.out.println(persistence.remove("/08/011"));
+        System.out.println(persistence.remove("/25/19"));
+        System.out.println(persistence.findBy("Alex;"));
+        System.out.println(persistence.findBy(";Isabella@"));
+        System.out.println(persistence.findBy("85;"));
+        System.out.println("========================================");
+
 
         }
     }
--- src/br/com/dio/persistence/IOFIlePersistence.java
+++ src/br/com/dio/persistence/IOFIlePersistence.java
@@ -1,6 +1,9 @@
 package br.com.dio.persistence;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class IOFIlePersistence implements FilePersistence {
 
@@ -23,7 +26,7 @@ public class IOFIlePersistence implements FilePersistence {
                 var fileWriter = new FileWriter(filePath, true);
                 var bufferedWriter = new BufferedWriter(fileWriter);
                 var printWriter = new PrintWriter(bufferedWriter);
-        ){
+        ) {
             printWriter.println(data);
             System.out.println("Dados escritos com sucesso em: " + filePath);
 
@@ -31,40 +34,87 @@ public class IOFIlePersistence implements FilePersistence {
             ex.printStackTrace();
         }
         // Implementation for writing data to a file
-        return  data;
+        return data;
     }
 
     @Override
     public boolean remove(final String sentence) {
         // Implementation for removing a sentence from a file
-        return true; // Assuming the removal was successful
+        var contentList = toListString();
+
+
+        if (contentList.stream().noneMatch(c -> c.contains(sentence))) return false;
+
+        clearFile();
+        contentList.stream().filter(c -> !c.contains(sentence));
+
+        return true;
     }
 
     @Override
     public String replace(final String oldContent, final String newContent) {
         // Implementation for replacing content in a file
-        return "Replaced '" + oldContent + "' with '" + newContent + "'";
+        var contentList = toListString();
+
+        if (contentList.stream().noneMatch(c -> c.contains(oldContent))) return "";
+        clearFile();
+        contentList.stream().map(c -> c.contains(oldContent) ? c.replace(oldContent, newContent) : c)
+                .forEach(this::write);
+        return newContent;
     }
 
     @Override
-    public String findAll() {
-        // Implementation for finding all content in a file
-        return "All content found";
-    }
 
-    @Override
-    public String findBy(final String sentence) {
-        // Implementation for finding a specific sentence in a file
-        return "Found: " + sentence;
-    }
-
-    private void clearFile() {
-        try (OutputStream outputStream = new FileOutputStream(currentDir + storeDir + "/" + fileName)) {
-            System.out.printf("Inicializando recursos (%s) \n", currentDir + storeDir + "/" + fileName);
-        } catch (IOException e) {
-
-            e.printStackTrace();
+        public String findAll () {
+            // Implementation for finding all content in a file
+            var content = new StringBuilder();
+            try (var reader = new BufferedReader(new FileReader(currentDir + storeDir + "/" + fileName))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line).append(System.lineSeparator());
+                }
+                System.out.println("Conteúdo lido com sucesso de: " + currentDir + storeDir + "/" + fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content.toString();
         }
 
+        @Override
+        public String findBy ( final String sentence){
+            // Implementation for finding a specific sentence in a file
+            String found = "";
+            try (var reader = new BufferedReader(new FileReader(currentDir + storeDir + "/" + fileName))) {
+                String line = reader.readLine();
+                while (line != null) {
+                    if (line.contains((sentence))) {
+                        found = line;
+                        break;
+                    }
+                    line = reader.readLine();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+
+            }
+            return found;
+        }
+
+    private List<String> toListString() {
+        var content = findAll();
+        return new ArrayList<>(Stream.of(content.split(System.lineSeparator())).toList());
+
     }
-}
+
+
+        private void clearFile() {
+            try (OutputStream outputStream = new FileOutputStream(currentDir + storeDir + "/" + fileName)) {
+                System.out.printf("Inicializando recursos (%s) \n", currentDir + storeDir + "/" + fileName);
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+
+        }
+    }
+
